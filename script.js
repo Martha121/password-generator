677db3831f3c4462ee29c072454dd48e45be6ad6
MGcommits <marthagamez211@gmail.com>
2020-12-19 12:05:13 -0600

    Added randon Upper Case function

--- script.js
+++ script.js
@@ -9,35 +9,53 @@ function generatePassword(){
     var lenghtPassword = prompt ('Choose the lenght of your password. Password should be between 8-125 characters');
     var capitalCharacter = confirm ('Do you want to include Capital characters');
     var lowercaseCharacter = confirm ('Do you want to include lower case characters');
+    var numberCharacter = confirm ('Do you want to include numbers');
     var specialCharacter = confirm ('Do you want to include special characters');
     var newPassword = "";
+ 
     while (newPassword.length<lenghtPassword ){
       var randType =Math.floor(Math.random()*5);
       switch(randType){
         case 1: // Upper case
           if(capitalCharacter){
-            newPassword = newPassword + "A";
+            newPassword = newPassword + getUpperCharacter();
           }
           break;
         case 2: //lowercase
+        if (lowercaseCharacter){
           newPassword = newPassword + "e";
+          }
           break;
         case 3: // number
+        if ( numberCharacter){
           newPassword = newPassword + "3";
+          }
           break;
         case 4: // Special charaxter
+        if (specialCharacter){
           newPassword = newPassword + "?";
+          }
           break;
-
       }
     }
+    
     return newPassword;
   }
   else{
     alert ('gracias')
     return "";
   }
+
 }
+
+// return  random Upper case character
+
+function getUpperCharacter(){
+  var charList= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var charIndex = Math.floor(Math.random()*26 )+1;
+  return charList.charAt(charIndex);
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
